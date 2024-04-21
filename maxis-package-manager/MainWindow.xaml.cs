a3c6023650cfd9d76a9ae4e87f807167ba7b7b50
owlks4 <115742200+owlks4@users.noreply.github.com>
2024-04-21 21:55:40 +0100

    Add project files.

---
+++ maxis-package-manager/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace maxis_package_manager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == true) {
+                Package p = new Package(openFileDialog.OpenFile());
+            }
+        }
+    }
+}
---
+++ maxis-package-manager/ReversibleBinaryRead.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maxis_package_manager
+{
+    public static class ReversibleBinaryRead
+    {
+        public static byte[] ReverseBytes(byte[] bytes) {
+            return bytes.Reverse().ToArray();
+        }
+
+        public static uint ReadUInt32(BinaryReader reader, bool reverse) {            
+            if (reverse){
+                return BitConverter.ToUInt32(ReverseBytes(BitConverter.GetBytes(reader.ReadUInt32())));
+            } else {
+                return reader.ReadUInt32();
+            }
+        }
+        public static int ReadInt32(BinaryReader reader, bool reverse)
+        {
+            if (reverse) {
+                return BitConverter.ToInt32(ReverseBytes(BitConverter.GetBytes(reader.ReadInt32())));
+            } else {
+                return reader.ReadInt32();
+            }
+        }
+
+        public static ushort ReadUInt16(BinaryReader reader, bool reverse)
+        {
+            if (reverse){
+                return BitConverter.ToUInt16(ReverseBytes(BitConverter.GetBytes(reader.ReadUInt16())));
+            } else {
+                return reader.ReadUInt16();
+            }
+        }
+
+        public static short ReadInt16(BinaryReader reader, bool reverse)
+        {
+            if (reverse){
+                return BitConverter.ToInt16(ReverseBytes(BitConverter.GetBytes(reader.ReadInt16())));
+            } else {
+                return reader.ReadInt16();
+            }
+        }
+
+        public static ulong ReadUInt64(BinaryReader reader, bool reverse)
+        {
+            if (reverse){
+                return BitConverter.ToUInt64(ReverseBytes(BitConverter.GetBytes(reader.ReadUInt64())));
+            } else {
+                return reader.ReadUInt64();
+            }
+        }
+
+        public static long ReadInt64(BinaryReader reader, bool reverse)
+        {
+            if (reverse) {
+                return BitConverter.ToInt64(ReverseBytes(BitConverter.GetBytes(reader.ReadInt64())));
+            } else {
+                return reader.ReadInt64();
+            }
+        }
+    }
+}
